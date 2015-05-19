b8d8c6be7e054dbed8fbf774a94f9759c9913d1c
baojuan <juanbao@meilishuo.com>
2015-05-19 13:00:09 +0800

    init

---
+++ CheckOrder/CheckOrder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CheckOrder
+//
+//  Created by baojuan on 15/5/19.
+//  Copyright (c) 2015年 baojuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CheckOrder/CheckOrder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CheckOrder
+//
+//  Created by baojuan on 15/5/19.
+//  Copyright (c) 2015年 baojuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
